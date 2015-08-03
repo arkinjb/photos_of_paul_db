bd6b39ab95860fa396e33fcd79ff5e0b19096ebb
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-08-03 13:19:40 -0400

    added full comment functionality

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,38 @@
+class CommentsController < ApplicationController
+  before_action :get_photo_id
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment = @photo.comments.create!(comment_params)
+    redirect_to photo_path(@photo)
+  end
+
+  def edit
+    @comment = Comment.find(params[:id])
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to photo_path(@photo)
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to photo_path(@photo)
+  end
+
+  private
+  def get_photo_id
+    @photo = Photo.find(params[:photo_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+end
