8f4c72683dd63724912ee77147b7e2cd52fcebde
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-08-04 10:54:33 -0400

    added search functionality to layout page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,34 +1,41 @@
 class PhotosController < ApplicationController
   load_and_authorize_resource
+  before_filter :check_for_cancel, :only => [:show]
+
+  def check_for_cancel
+    if params[:commit] == "Cancel"
+      redirect_to root_url
+    end
+  end
 
   def index
     # @photos = Photo.all.sort_by {|photo| photo[:created_at]}.reverse.first(12)
     @random_photo = Photo.find(1 + Random.rand(Photo.all.length))
-    @photos = Photo.order("created_at DESC").paginate(page: params[:page], per_page: 5)
+    @photos = paginate_order(Photo)
+
+    if params[:search]
+      @photos = paginate_order(Photo.search(params[:search]))
+    else
+      @photos = paginate_order(Photo.all)
+    end
   end
 
   def show
-
     @comments = @photo.comments.all
-
   end
 
   def new
-
   end
 
   def create
     @photo = Photo.create!(photo_params.merge({user_id: current_user.id}))
-
     redirect_to photo_path(@photo)
   end
 
   def edit
-
   end
 
   def update
-
     @photo.update(photo_params)
     redirect_to photo_path(@photo)
   end
@@ -36,6 +43,14 @@ class PhotosController < ApplicationController
   def destroy
     @photo.destroy
     redirect_to root_url
+  end
+
+  def self.search(query)
+    where("title like ?", "%#{query}%")
+  end
+
+  def paginate_order(query)
+    query.order("created_at DESC").paginate(page: params[:page], per_page: 5)
   end
 
   private
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,9 @@
 class Photo < ActiveRecord::Base
   belongs_to :user
   has_many :comments, dependent: :destroy
+
+  def self.search(query)
+    where("title ILIKE ?", "%#{query}%")
+  end
+
 end
