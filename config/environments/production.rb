76bc61db2489aa5a675b4406953bcb5e0dc23f44
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-08-06 16:16:41 -0400

    added region hostname and url to s3 credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,11 +78,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.paperclip_defaults = {
-  :storage => :s3,
-  :s3_credentials => {
-    :bucket => ENV['S3_BUCKET_NAME'],
-    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['AWS_BUCKET'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      region: 'us-west-2',
+      s3_host_name: 's3-us-west-2.amazonaws.com',
+      url: ":s3_domain_url"
     }
   }
 end
