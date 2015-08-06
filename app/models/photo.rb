33b2309f51c20935fded9233d61dcabc3f185f36
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-08-06 15:03:01 -0400

    image is uploaded. not showing on page

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,9 @@ class Photo < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   has_many :favorite_photos, dependent: :destroy
 
-  has_attached_file :image
+  has_attached_file :image {
+    :url => "photos/images/000/000/016/original/:image_file_name"
+  }
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 
   def self.search(query)
